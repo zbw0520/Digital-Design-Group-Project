f401f50cb463762f301cc63be53b83e81d402de7
Bowen Zhang <1620419196@qq.com>
2020-02-26 00:42:48 +0000

    update cmdProc.vhd

--- peak_detector.srcs/sources_1/new/cmdProc.vhd
+++ peak_detector.srcs/sources_1/new/cmdProc.vhd
@@ -61,29 +61,30 @@ entity cmdProc is
 end cmdProc;
 
 architecture Behavioral of cmdProc is
-signal oe, fe, valid : std_logic;
-signal data_rx, data_tx : std_logic_vector(7 downto 0);
-signal numWords_bcd_internal : std_logic_vector(11 downto 0);
+signal oe, fe, valid, start_reg, rxdone_reg: std_logic;   --内部寄存器信号单比特
+signal data_rx, data_tx : std_logic_vector(7 downto 0); --内部寄存器信号多比特
+signal numWords_bcd_reg : BCD_ARRAY_TYPE(2 downto 0);
+signal memory : CHAR_ARRAY_TYPE(998 downto 0);  --memory for maximum 999 bytes
+signal counter, counter_full: std_logic_vector(9 downto 0); -- maximum 999
 
-
-TYPE state_type IS (INIT, RX_INIT, RX_A, RX_P, RX_L, RX_A_1, RX_A_2, RX_A_3);   --define states in the state machine
-SIGNAL curState, nextState: state_type;
+type state_type IS (INIT, RX_INIT, RX_A, RX_P, RX_L, RX_A_1, RX_A_2, dataConsumer_communication_A, tx_A_init, tx_A);   --定义状�?�机中的变量
+signal curState, nextState: state_type; --状�?�变�???????
 
 begin
--------------------------------------------------------------------
+--------------主状态机过程--------------------------
 
-combi_nextState: PROCESS(curState, valid)
+combi_nextState: PROCESS(curState, valid, data_rx, seqDone, dataReady)
 begin
     case curState is
-        when INIT =>
+        when INIT =>    --状�?�机初始状�??
             if valid = '1' then
                 nextState <= RX_INIT;
             else
                 nextState <= INIT;
             end if;
 
-        when RX_INIT =>
-            if fe = '1' or oe = '1' then
+        when RX_INIT => --状�?�机接收状�??
+            if fe = '1' and valid = '1' then
                 nextState <= RX_INIT;
             elsif data_rx = "01000001" or data_rx = "01100001" then --A or a
                 nextState <= RX_A;
@@ -96,47 +97,65 @@ begin
             end if;             
 
         when RX_A =>
-            if fe = '1' or oe = '1' then
+            if fe = '1' and valid = '1' then
                 nextState <= RX_INIT;
-            elsif data_rx < "00110000" or data_rx > "00111001" then --0 = "00110000" 9 = "00111001"
-                nextState <= RX_INIT;                  
+            elsif data_rx >= "00110000" and data_rx <= "00111001" and valid = '1' then --0 = "00110000" 9 = "00111001"
+                nextState <= RX_A_1;                  
             else
-                nextState <= RX_A_1;
-                numWords_bcd_internal <= numWords_bcd_internal(11 downto 4) & data_rx(3 downto 0);
+                nextState <= RX_A;
             end if;
         when RX_A_1 =>
-            if fe = '1' or oe = '1' then
+            if fe = '1' and valid = '1' then
                 nextState <= RX_INIT;
-            elsif data_rx < "00110000" or data_rx > "00111001" then  --0 = "00110000" 9 = "00111001"
-                nextState <= RX_INIT;                  
+            elsif data_rx >= "00110000" and data_rx <= "00111001" and valid = '1' then  --0 = "00110000" 9 = "00111001"
+                nextState <= RX_A_2;                  
             else
-                nextState <= RX_A_2;
-                numWords_bcd_internal <= numWords_bcd_internal(11 downto 8) & data_rx(3 downto 0) & numWords_bcd_internal(3 downto 0);
+                nextState <= RX_A_1;
             end if;
         when RX_A_2 =>
-            if fe = '1' or oe = '1' then
+            if fe = '1' and valid = '1' then
                 nextState <= RX_INIT;
-            elsif data_rx < "00110000" or data_rx > "00111001" then  --0 = "00110000" 9 = "00111001"
-                nextState <= RX_INIT;                  
+            elsif data_rx >= "00110000" and data_rx <= "00111001" and valid = '1' then  --0 = "00110000" 9 = "00111001"
+                nextState <= dataConsumer_communication_A;                  
             else
-                nextState <= RX_A_3;
-                numWords_bcd <= data_rx(3 downto 0) & numWords_bcd_internal(7 downto 0);
-                start <= '1';
+                nextState <= RX_A_2;
+            end if;
+        when dataConsumer_communication_A =>
+            if seqDone = '1' then
+                nextState <= tx_A_init;
+            elsif dataReady = '1' then
+                nextState <= dataConsumer_communication_A;
+            else
+                nextState <= dataConsumer_communication_A;
+            end if;
+        when tx_A_init =>
+            if txdone = '0' then
+                nextState <= tx_A;
+            else
+                nextState <= tx_A_init;
+            end if;
+        when tx_A =>
+            if counter = counter_full then
+                nextState <= INIT;
+            elsif txdone = '1' then
+                nextState <= tx_A;
+            else
+                nextState <= tx_A;
             end if;
         when others =>
             nextState <= INIT;
     end case;
 end process; 
--------------------------------------------------------------------
+---------------状�?�变量赋值控制过�????----------------------------------
 seq_state: process (clk, reset)
 begin
-  if reset = '0' then
+  if reset = '1' then
     curState <= INIT;
   elsif clk'event and clk='1' then
     curState <= nextState;
   end if;
 end process; -- seq
--------------------------------------------------------------------
+------------信号寄存控制过程--------------------------
 registers: process(clk)
 begin
     if clk'event and clk='1' then
@@ -151,10 +170,59 @@ begin
         valid <= valid;
     end if;
 end process;
--------------------------------------------------------------------
-combi_out: process(curState)
+----------输出信号控制过程--------------------------
+combi_out: process(clk, curState, nextState, dataReady, seqDone, txdone)
 begin
+    if clk'event and clk='1' then
+        if reset = '1' then
+            rxdone_reg <= '0';
+            start_reg <= '0';
+            numWords_bcd_reg(0) <= "0000";
+            numWords_bcd_reg(1) <= "0000";
+            numWords_bcd_reg(2) <= "0000";
+            counter <= "0000000000";
+            txnow <= '0';
+        else
+            if nextState = RX_INIT and curState = INIT then
+                rxdone_reg <= '1';
+            elsif nextState = RX_A and curState = RX_INIT then
+                rxdone_reg <= '1';
+            elsif nextState = RX_A_1 and curState = RX_A then
+                numWords_bcd_reg(2) <= data_rx(3 downto 0);
+                rxdone_reg <= '1';
+            elsif nextState = RX_A_2 and curState = RX_A_1 then
+                numWords_bcd_reg(1) <= data_rx(3 downto 0);
+                start_reg <= '0';
+                rxdone_reg <= '1';
+            elsif nextState = dataConsumer_communication_A and curState = RX_A_2 then
+                numWords_bcd_reg(0) <= data_rx(3 downto 0);
+                start_reg <= '1';
+            elsif nextState = tx_A_init and curState <= dataConsumer_communication_A then
+                start_reg <= '0';
+            elsif curState = dataConsumer_communication_A and dataReady = '1' then
+                memory(to_integer(unsigned(counter))) <= byte;
+                counter <= std_logic_vector(unsigned(counter) + "0000000001");
+            elsif curState = tx_A_init and nextState = tx_A_init then
+                counter_full <= counter;
+            elsif curState = tx_A_init then
+                counter <= "0000000000";
+                txData <= memory(0);
+                txnow <= '1';
+            elsif curState = tx_A and txdone = '1' then
+                txData <= memory(to_integer(unsigned(counter)));
+                txnow <= '1';
+                counter <= std_logic_vector(unsigned(counter) + "0000000001");
+            else
+                rxdone_reg <= rxdone_reg;
+                start_reg <= start_reg;
+                numWords_bcd_reg <= numWords_bcd_reg;
+                counter <= counter;
+                txnow <= '0';
+            end if;
+        end if;
+    end if;
 end process; -- combi_output
--------------------------------------------------------------------
-
+numWords_bcd <= numWords_bcd_reg;
+rxdone <= rxdone_reg;
+start <= start_reg;
 end Behavioral;
